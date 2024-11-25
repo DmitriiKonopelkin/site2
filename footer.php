eb5883ba71cd973f92d09f9e9cd22ae9a778d754
DmitriiKonopelkin <dm.konopelkin@gmail.com>
2024-11-25 10:38:22 +0300

    add bootstrap

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer class="bg-dark text-white py-3 mt-5">
+    <div class="container">
+        <p>© <?php echo date('Y'); ?> РСТ. Все права защищены.</p>
+    </div>
+</footer>
---
+++ header.php
@@ -0,0 +1,35 @@
+<header>
+<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">Ремонт складской техники</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Главная</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about_us.php">О нас</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contacts.php">Контакты</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="info.php">Информация</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="news.php">Новости</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="history.php">История</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="for_clients.php">Для клиентов</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    </header>
